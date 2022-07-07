ddf8ce7812a6a9a3e780d02c4807aaeee4f0d86b
czmud <caleb.zmuda@protonmail.com>
2022-07-07 14:18:28 -0700

    upload playground

---
+++ flask/fundamentals/playground/server.py
@@ -0,0 +1,13 @@
+from email.policy import default
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/', defaults={'num': 0, 'color':"pink"})
+@app.route('/play/', defaults={'num': 3, 'color':"pink"})
+@app.route('/play/<int:num>/', defaults={'color':"blue"})
+@app.route('/play/<int:num>/<color>/')
+def play1(num, color):
+    return render_template("index.html", block_color=color, block_count=num)
+
+if __name__=="__main__":
+    app.run(debug=True, port=5000)
---
+++ flask/fundamentals/playground/static/js/script.js
@@ -0,0 +1,5 @@
+
+
+color = document.getElementById("block-color-holder").innerHTML
+document.getElementById("header-color").innerHTML =".block-color{background-color: "+color+";}"
+document.getElementById("block-color-holder").remove()
