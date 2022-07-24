03b44c47f5f4b0438a4801833d8d172539b72fed
czmud <caleb.zmuda@protonmail.com>
2022-07-24 14:40:24 -0700

    modify array position swapping to python syntax

--- fundamentals/extras/selection_sort.py
+++ fundamentals/extras/selection_sort.py
@@ -1,19 +1,22 @@
 
 
-test_array = [5, 6, 9, 4, 0, 1, 1, 22, 17, 2, 1, 5]
+test_array1 = [5, 6, 9, 4, 0, 1, 1, 22, 17, 2, 1, 5]
+test_array2 = [7]
+test_array3 = [9, 5, 5, 5, 5, 5, 5]
 
 def selection_sort(array):
-    
+    if len(array) == 1:
+        return array
+
     for i in range(0,len(array)-1):
         temp = array[i]
         k = i
         for j in range(i+1,len(array)):
-            if array[j] < temp:
+            if array[j] < array[k]:
                 k = j
-                temp = array[k]
-        array[k] = array[i]
-        array[i] = temp
-
+        array[k], array[i] = array[i], array[k]
     return array
 
-print(selection_sort(test_array))
+print(selection_sort(test_array1))
+print(selection_sort(test_array2))
+print(selection_sort(test_array3))
